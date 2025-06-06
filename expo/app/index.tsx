65d9b5d3f014757ecc85c29a8c2d22a6a2654871
yiminghe <yiminghe@gmail.com>
2025-06-07 01:23:25 +0800

    add demo

--- expo/app/index.tsx
+++ expo/app/index.tsx
@@ -1,15 +1,23 @@
 /// <reference types="react/canary" />
 
-import React from 'react';
+import React, { cache } from 'react';
 import { Text, View } from 'react-native';
-import { getNextContext, withPageMiddlewares } from '../source/expo';
+// import { getNextContext, withPageMiddlewares } from '../source/expo';
 
-export default withPageMiddlewares([])(async (...args) => {
-  console.log('arguments', args);
-  console.log('context', getNextContext());
+const cached = cache<() => { x?: string }>(() => ({}));
+
+const Page = async () => {
+  cached().x = 'hello';
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20, color: 'blue' }}>Hello, Expo!</Text>
+      <Text style={{ fontSize: 20, color: 'blue' }}>
+        Hello, Expo!: {cached().x}
+      </Text>
     </View>
   );
-});
+};
+
+// withPageMiddlewares([])
+export default Page;
